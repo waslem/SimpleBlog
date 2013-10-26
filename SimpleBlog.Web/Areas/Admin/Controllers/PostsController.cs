9498cbf77783770601a1e0ba521f90c68435fd19
Jamie van Walsum <jvw@westnet.com.au>
2013-10-26 15:44:52 +0800

    initial commit, added admin area and auth and posts controllers, updated packages to mvc 5

---
+++ SimpleBlog.Web/Areas/Admin/Controllers/PostsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Web.Areas.Admin.Controllers
+{
+    public class PostsController : Controller
+    {
+        //
+        // GET: /Admin/Posts/
+
+        public ActionResult Index()
+        {
+            return Content("Admin posts!");
+        }
+
+    }
+}
---
+++ SimpleBlog.Web/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        //
+        // GET: /Auth/
+        public ActionResult Index()
+        {
+            return Content("Login!");
+        }
+    }
+}
