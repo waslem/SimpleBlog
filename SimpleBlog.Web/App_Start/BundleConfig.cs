6d5a379cbf29548cd7619ea44f5075d42b12e924
Jamie van Walsum <jvw@westnet.com.au>
2013-11-20 15:04:51 +0800

    added bundles and js

---
+++ SimpleBlog.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SimpleBlog.Web.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/admin/styles")
+                .Include("~/Content/bootstrap.css")
+                .Include("~/Content/Admin.css"));
+
+            bundles.Add(new StyleBundle("~/styles")
+                .Include("~/Content/bootstrap.css")
+                .Include("~/Content/Site.css"));
+
+            bundles.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/scripts/jquery-{version}.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js")
+                .Include("~/scripts/modernizr-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts")
+                .Include("~/scripts/jquery-{version}.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js")
+                .Include("~/scripts/modernizr-{version}.js"));
+        }
+    }
+}
