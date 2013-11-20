25e54f6ae9dbd66c804c3a952eed10a17d8906b5
Jamie van Walsum <jvw@westnet.com.au>
2013-11-20 15:41:28 +0800

    added selectedTab attribute, updated admin views

--- SimpleBlog.Web/Areas/Admin/Controllers/UsersController.cs
+++ SimpleBlog.Web/Areas/Admin/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleBlog.Web.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,7 @@ using System.Web.Mvc;
 namespace SimpleBlog.Web.Areas.Admin.Controllers
 {
     [Authorize(Roles="admin")]
+    [SelectedTab("users")]
     public class UsersController : Controller
     {
         //
---
+++ SimpleBlog.Web/Infrastructure/SelectedTabAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlog.Web.Infrastructure
+{
+    // only allow the attribute to be used on classes and methods
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class SelectedTabAttribute : ActionFilterAttribute
+    {
+        private readonly string _selectedTab;
+
+        public SelectedTabAttribute(string selectedTab)
+        {
+            _selectedTab = selectedTab;
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            filterContext.Controller.ViewBag.SelectedTab = _selectedTab;
+
+        }
+    }
+}
