c2558a88cb8a3ee0dd73eb984236fd11bba701c8
Jamie van Walsum <jvw@westnet.com.au>
2013-11-26 13:13:05 +0800

    Added delete/restore/trash posts Added slugify component for slug

--- SimpleBlog.Web/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlog.Web/Areas/Admin/Controllers/PostsController.cs
@@ -33,5 +33,108 @@ namespace SimpleBlog.Web.Areas.Admin.Controllers
                     Posts = new PagedData<Post>(currentPostsPage, totalPostCount, page, PostsPerPage)
                 });
         }
+
+        public ActionResult New()
+        {
+            return View("Form", new PostsForm
+            {
+                IsNew = true
+            });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            //var post = Database.Session.Query<Post>().FirstOrDefault(p => p.Id == id);
+            var post = Database.Session.Load<Post>(id);
+
+            if (post == null)
+                return HttpNotFound();
+
+            return View("Form", new PostsForm
+            {
+                IsNew = false,
+                PostId = post.Id,
+                Content = post.Content, 
+                Slug = post.Slug,
+                Title = post.Title
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Form(PostsForm form)
+        {
+            form.IsNew = form.PostId == null;
+            
+            if (!ModelState.IsValid)
+                return View(form);
+
+            Post post;
+            if (form.IsNew)
+            {
+                post = new Post
+                {
+                    Created = DateTime.Now,
+                    User = Auth.User
+                };
+            }
+            else
+            {
+                post = Database.Session.Load<Post>(form.PostId);
+                if (post == null)
+                    return HttpNotFound();
+
+                post.Updated = DateTime.Now;
+            }
+
+            post.Title = form.Title;
+            post.Slug = form.Slug;
+            post.Content = form.Content;
+
+            Database.Session.SaveOrUpdate(post);
+
+            return Redirect("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Trash(int id)
+        {
+            var post = Database.Session.Load<Post>(id);
+            if (post == null)
+                return HttpNotFound();
+
+            post.Deleted = DateTime.Now;
+            Database.Session.Update(post);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            var post = Database.Session.Load<Post>(id);
+            if (post == null)
+                return HttpNotFound();
+
+            Database.Session.Delete(post);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Restore(int id)
+        {
+            var post = Database.Session.Load<Post>(id);
+            if (post == null)
+                return HttpNotFound();
+
+            post.Deleted = null;
+            Database.Session.Update(post);
+
+            return RedirectToAction("Index");
+        }
 	}
 }
